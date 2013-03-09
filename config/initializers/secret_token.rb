323ea6eb9d2a53c6c6a88b537e545828003ba14a
jizilong <jizilong1988@163.com>
2013-03-09 16:32:50 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thebeast::Application.config.secret_token = '314de63061060182c135e8f6bc26ddadadfb38ad8306f68043f74ce724e0015805e579f3bbc9c5fc19051b74cd2d8cc2a3612365e164c7234f5df9e7b3acfc29'

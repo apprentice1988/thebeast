52b2ba8baf40427c94e449f6c992f536d12682b3
apprentice1988 <jizilong1988@163.com>
2013-06-01 23:40:42 +0800

    up

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :email, :fixlinephone, :mobilephone, :username, :password, :password_confirmation, :subscription, :province,
+  attr_accessible :email, :fixlinephone, :mobilephone, :username,:subscription, :province,
                   :city, :zone, :address, :postcode
   has_one :cart, dependent: :destroy
   has_many :likes, dependent: :destroy
@@ -18,13 +18,11 @@ class User < ActiveRecord::Base
   before_save { |user| user.email.downcase! }
   before_save :create_remember_token
 
-  validates :username, presence: true, length: {in: 3..50}
+  #validates :username, presence: true, length: {in: 3..50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
   									uniqueness: {case_sensitive: false}
-  validates :mobilephone, presence: true, length: {is: 11}, numericality: {:only_integer => true}
-  validates :password, presence: true, length: {minimum: 6}
-  validates :password_confirmation, presence: true
+  #validates :mobilephone, presence: true, length: {is: 11}, numericality: {:only_integer => true}
 
   private
     def create_remember_token 
